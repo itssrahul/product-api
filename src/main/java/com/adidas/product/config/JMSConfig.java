5978daf243b8ff32c7a31e026e31d8fd8e4f9e6a
Rahul Soni <therimpact@gmail.com>
2019-04-15 01:41:35 +0530

    Removed Unnecessary import

--- src/main/java/com/adidas/product/config/JMSConfig.java
+++ src/main/java/com/adidas/product/config/JMSConfig.java
@@ -4,7 +4,6 @@ package com.adidas.product.config;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.annotation.EnableJms;
 
 import javax.jms.Queue;
 
